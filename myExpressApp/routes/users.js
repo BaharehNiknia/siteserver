77308696fc9e45c5b49ee31f195f089ebb530830
BaharehNiknia <Bahareh.Niknia@gmail.com>
2018-12-25 08:22:37 -0800

    in way of connecting client and server

--- myExpressApp/routes/users.js
+++ myExpressApp/routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//  res.send('respond with a resource');
+
+res.json([{
+  id: 1,
+  username: "samsepi0l"
+}, {
+  id: 2,
+  username: "D0loresH4ze"
+}]);
 });
+ // And insert something like this instead:
+ 
+
 
 module.exports = router;
